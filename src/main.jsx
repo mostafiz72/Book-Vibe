57728bbdf96125fe851d2ae6f67a067a1c75a6f1
Hera <mostafizhera@gmail.com>
2024-11-01 21:05:34 +0600

    added the default page and all setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import Error from './Conponents/Error.jsx'
+import Books from './Conponents/Books.jsx';
+import Default from './Conponents/Default.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+       {
+        path: "/",
+        element: <Default />,
+       },
+       {
+        path: "/books",
+        element: <Books />,
+       },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
